5b30aff1cc43daaf6d50e0279c7a2d4f824ae2ea
Keerthivasan Gurusamy <keerthivasan000071@gmail.com>
2023-11-02 12:23:42 +0530

    No Acne Problems

--- src/camera.h
+++ src/camera.h
@@ -13,6 +13,7 @@ class camera
         double aspect_ratio = 1.0;
         int image_width = 100;
         int samples_per_pixel = 10;
+        int max_depth = 10;
         
         void render(const hittable& world)
         {
@@ -29,7 +30,7 @@ class camera
                     for(int sample=0; sample < samples_per_pixel; sample++)
                     {
                         ray r = get_ray(i, j);
-                        pixel_color += ray_color(r, world);
+                        pixel_color += ray_color(r, max_depth, world);
                     }
                     write_color(std::cout, pixel_color, samples_per_pixel);
                 }
@@ -89,16 +90,20 @@ class camera
             return (px * pixel_delta_u) + (py * pixel_delta_v);
         }
 
-        color ray_color(const ray& r, const hittable& world)
+        color ray_color(const ray& r, int depth, const hittable& world)
         {
             hit_record rec;
-            if(world.hit(r, interval(0, infinity), rec))
+            if(depth <= 0)
             {
-                vec3 direction = random_on_hemisphere(rec.normal);
-                return 0.5 * ray_color(ray(rec.p, direction), world);
+                return color(0,0,0);
+            }
+            if(world.hit(r, interval(0.001, infinity), rec))
+            {
+                vec3 direction = rec.normal + random_unit_vector();
+                return 0.2 * ray_color(ray(rec.p, direction), depth-1, world);
             }
             vec3 unit_direction = unit_vector(r.direction());
-            auto a = 0.5*(unit_direction.y() + 1.0);
+            auto a = 0.9*(unit_direction.y() + 1.0);
             return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5,0.7,1.0); //Background color for the image using Linear Interpolation
         }
 };
